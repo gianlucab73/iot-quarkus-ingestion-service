82e986ba89786c1964d0db8f939a6f0051e5a35e
matteodefinis <matteopiodefinis@gmail.com>
2024-07-16 12:09:37 +0200

    Change ObjectMapper to a custom one
    The ObjectMapper was replaced by SensorOutputMessageMapper to add support to JavaTimeModule required for LocalDateTime timestamps. Also the application now saves the received messages in TimescaleDB

--- src/main/java/it/alten/KafkaConsumer.java
+++ src/main/java/it/alten/KafkaConsumer.java
@@ -1,19 +1,23 @@
 package it.alten;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import it.alten.mapper.SensorOutputMessageMapper;
 import it.alten.model.SensorOutputMessage;
+import it.alten.repository.SensorOutputMessageRepository;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 
 @ApplicationScoped
 public class KafkaConsumer {
+    @Inject
+    SensorOutputMessageRepository sensorOutputMessageRepository;
+
     @Incoming("kafka-channel-in")
     public void consume(String message) {
-        try {
-            SensorOutputMessage sensorOutputMessage = new ObjectMapper().readValue(message, SensorOutputMessage.class);
-            System.out.println("Deserialized message: " + sensorOutputMessage);
-        } catch (Exception e) {
-            System.out.println("Error deserializing message: " + e.getMessage());
-        }
+        SensorOutputMessage sensorOutputMessage = new SensorOutputMessageMapper().readValue(message);
+
+        sensorOutputMessageRepository.save(sensorOutputMessage);
+
+        System.out.println("Deserialized message: " + sensorOutputMessage);
     }
 }
--- src/main/java/it/alten/model/SensorOutputMessage.java
+++ src/main/java/it/alten/model/SensorOutputMessage.java
@@ -1,12 +1,25 @@
 package it.alten.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
+@Entity
 @Data
 public class SensorOutputMessage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
     @JsonProperty("serial_number")
+    @Column(nullable = false)
     private String serialNumber;
     @JsonProperty("data")
+    @Column(nullable = false)
     private double data;
+    @JsonProperty("timestamp")
+    @Column(nullable = false)
+    private LocalDateTime timestamp;
 }
