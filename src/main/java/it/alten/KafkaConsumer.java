45e93598dea7edf46049fe8899f9957d08174b11
matteodefinis <matteopiodefinis@gmail.com>
2024-07-18 15:12:57 +0200

    Remove unnecessary EntityManager

--- src/main/java/it/alten/KafkaConsumer.java
+++ src/main/java/it/alten/KafkaConsumer.java
@@ -7,16 +7,12 @@ import it.alten.repository.SensorOutputMessageRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.event.Observes;
 import jakarta.inject.Inject;
-import jakarta.persistence.EntityManager;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 
 @ApplicationScoped
 public class KafkaConsumer {
     @Inject
     SensorOutputMessageRepository sensorOutputMessageRepository;
-
-    @Inject
-    EntityManager entityManager;
 
     @Incoming("kafka-channel-in")
     public void consume(String message) {
@@ -29,6 +25,6 @@ public class KafkaConsumer {
     }
 
     void onStart(@Observes StartupEvent ev) {
-        entityManager.createNativeQuery("SELECT create_hypertable('sensor_output_message', 'timestamp', if_not_exists => TRUE);").getSingleResult();
+        sensorOutputMessageRepository.createHypertable();
     }
 }
--- src/main/java/it/alten/repository/SensorOutputMessageRepository.java
+++ src/main/java/it/alten/repository/SensorOutputMessageRepository.java
@@ -2,9 +2,12 @@ package it.alten.repository;
 
 import it.alten.model.SensorOutputMessage;
 import it.alten.model.id.SensorOutputMessageId;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SensorOutputMessageRepository extends CrudRepository<SensorOutputMessage, SensorOutputMessageId> {
+    @Query(value="SELECT create_hypertable('sensor_output_message', 'timestamp')")
+    Object createHypertable();
 }
